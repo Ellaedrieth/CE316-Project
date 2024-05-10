609201f494ee84ea3b4ceb7aa49da722a9180f7f
aycadernek <aydernek@gmail.com>
2024-05-10 21:58:41 +0300

    New project functionality.

--- src/main/java/com/example/ce316project/FileScanner.java
+++ src/main/java/com/example/ce316project/FileScanner.java
@@ -24,5 +24,25 @@ public class FileScanner{
         }
         return configFilesList;
     }
+    public HashSet<String> scanProjectFiles(String directoryPath) throws IllegalAccessException {
+        HashSet<String> projectFilesList = new HashSet<>();
+        File directory = new File(directoryPath);
+
+        if (!directory.exists() || !directory.isDirectory()) {
+            throw new IllegalAccessException("Directory does not exist or it is not a directory!");
+        }
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            throw new IllegalArgumentException("No files found in the specified directory.");
+        }
+
+        for (File file : files) {
+            if (file.isFile() && file.getName().toLowerCase().endsWith(".txt")) {
+                projectFilesList.add(file.getAbsolutePath());
+            }
+        }
+        return projectFilesList;
+    }
 }
 
--- src/main/java/com/example/ce316project/ProjectController.java
+++ src/main/java/com/example/ce316project/ProjectController.java
@@ -3,6 +3,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
@@ -10,25 +11,25 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.w3c.dom.Text;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
 public class ProjectController implements Initializable {
     public final String CONFIG_FILE_PATH = "src/main/resources/com/example/ce316project/";
+    public final String Project_File_Path = "src/main/resources/com/example/ce316project/";
     @FXML
-    Button submissionZipBtn, outputBtn;
+    Button submissionZipBtn, outputBtn , projectGenBtn;
     @FXML
-    TextField submissionTextField,outputTextField;
+    TextField submissionTextField,outputTextField,projectNameTextField;
     @FXML
     ChoiceBox<String> projectConfigMenu;
     //to access the object from the ConfigController class; reference
     HashSet<String> progLanItems = new HashSet<>();
     static HashSet<String> newLangItems = new HashSet<>();
+    static HashSet<String> ProjectItems = new HashSet<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -36,6 +37,8 @@ public class ProjectController implements Initializable {
         projectConfigMenu.getItems().addAll(scanConfigFiles());
         submissionZipBtn.setOnAction(event -> chooseSubmissionPath());
         outputBtn.setOnAction(event -> chooseOutputFile());
+        projectGenBtn.setOnAction(event -> projectGenerateAct());
+
     }
     public static String takeLang(String newConfigFileName){
         String languageValue = "";
@@ -105,4 +108,82 @@ public class ProjectController implements Initializable {
             outputTextField.setText(selectedFile.getAbsolutePath());
         }
     }
+
+    public void saveProject(String projectName, String configName, String zipFilePath, String outputFile) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("src/main/resources/com/example/ce316project/"+ projectName + ".txt")) {
+            StringBuilder newProjectData = new StringBuilder(); //using StringBuilder is more effective from the point of memory usage
+            newProjectData.append("Project Name: ").append(projectName).append("\n");
+            newProjectData.append("Configuration Name: ").append(configName).append("\n");
+            newProjectData.append("Zip File Path: ").append(zipFilePath).append("\n");
+            newProjectData.append("Output File: ").append(outputFile).append("\n");
+
+            fileOutputStream.write(newProjectData.toString().getBytes(StandardCharsets.UTF_8));
+
+            Alert successAlert = new Alert(Alert.AlertType.INFORMATION);
+            successAlert.setTitle("Success");
+            successAlert.setHeaderText("Project saved successfully!");
+            successAlert.showAndWait();
+
+        } catch (IOException e) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setTitle("Error");
+            errorAlert.setHeaderText("An error occurred while saving the project.");
+            errorAlert.setContentText(e.getMessage());
+            errorAlert.showAndWait();
+        }
+    }
+    public void projectGenerateAct() {
+        //Values taken from new project screen
+        String project_name = projectNameTextField.getText();
+        String config_name = projectConfigMenu.getSelectionModel().getSelectedItem();
+        String zip_file_path = submissionTextField.getText();
+        String output_file = outputTextField.getText();
+        scanProjectFiles();
+
+        if (project_name.isEmpty() || projectConfigMenu.getValue()==null || zip_file_path.isEmpty() || output_file.isEmpty()) {
+            Alert validationError = new Alert(Alert.AlertType.ERROR);
+            validationError.setTitle("Validation Error");
+            validationError.setHeaderText("Please fill in all required fields.");
+            validationError.showAndWait();
+        } else if (ProjectItems.contains(project_name)) {
+            Alert validationError = new Alert(Alert.AlertType.ERROR);
+            validationError.setTitle("Duplicate Error");
+            validationError.setHeaderText("This project already exists.");
+            validationError.showAndWait();
+        } else{
+            //save project
+            saveProject(project_name, config_name, zip_file_path, output_file);
+        }
+    }
+    public static String takeProjectName(String newProjectFileName){
+        String ProjectVal = "";
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(newProjectFileName))){
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                if(line.startsWith("Project Name: ")){
+                    ProjectVal = line.substring("Project Name: ".length()).trim();
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ProjectVal;
+    }
+
+    public HashSet<String> scanProjectFiles(){
+        FileScanner fileScannerForProject = new FileScanner();
+        try {
+            HashSet<String> projectFilesList = fileScannerForProject.scanProjectFiles(Project_File_Path);
+            for(String txtFile : projectFilesList){
+                String p_nameTaken = takeProjectName(txtFile);
+                ProjectItems.add(p_nameTaken);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return ProjectItems;
+    }
+
+
 }
