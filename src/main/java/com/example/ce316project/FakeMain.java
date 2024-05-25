b45703808556b9130b6885b441391fc57d9cb6f4
bsraayilmz <bsraayilmz35@gmail.com>
2024-05-25 15:29:52 +0300

    Ikonli deleted and executable jar created
    Co-Authored-By: Sedat Nail Ersoy <93867521+Ellaedrieth@users.noreply.github.com>
    Co-Authored-By: aycadernek <121360043+aycadernek@users.noreply.github.com>

---
+++ src/main/java/com/example/ce316project/FakeMain.java
@@ -0,0 +1,7 @@
+package com.example.ce316project;
+
+public class FakeMain {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/com/example/ce316project/Main.java
+++ src/main/java/com/example/ce316project/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
